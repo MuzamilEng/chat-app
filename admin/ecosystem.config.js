47713a287b5d8b93d89f203086732d93eb2e410e
root <root@server.girls2dream.de>
2024-08-12 03:02:22 -0400

    creds

--- admin/ecosystem.config.js
+++ admin/ecosystem.config.js
@@ -1,21 +1,55 @@
 require('dotenv').config();
 
+// module.exports = {
+//   apps: [
+//     {
+//       name: "xchat-admin",
+//       script: "node_modules/next/dist/bin/next",
+//       args: "start -p 8082",
+//       instances: 1,
+//       autorestart: true,
+//       watch: false,
+//       max_memory_restart: "1G",
+//       env: {
+//         NODE_ENV: process.env.NODE_ENV || "development",
+//       },
+//       env_production: {
+//         NODE_ENV: "production",
+//       },
+//     },
+//   ],
+// };
+
+
+// module.exports = {
+//   apps: [
+//     {
+//       name: "xchat-admin",
+//       script: "node_modules/.bin/next",
+//       args: "start -p 8082",
+//       cwd: "/root/chat-app/admin",
+//       env: {
+//         NODE_ENV: "production",
+//       },
+//     },
+//   ],
+// };
+
 module.exports = {
   apps: [
     {
       name: "xchat-admin",
-      script: "node_modules/next/dist/bin/next",
-      args: "start -p 8082",
+      script: "node_modules/.bin/next",
+      args: "start -p 5002",
+      cwd: "/root/chat-app/admin",
+      env: {
+        NODE_ENV: "production",
+      },
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: "1G",
-      env: {
-        NODE_ENV: process.env.NODE_ENV || "development",
-      },
-      env_production: {
-        NODE_ENV: "production",
-      },
     },
   ],
 };
+
--- user/ecosystem.config.js
+++ user/ecosystem.config.js
@@ -1,20 +1,42 @@
-module.exports = {
+// module.exports = {
+//     apps: [
+//       {
+//         name: "xchat-front-office",
+//         script: "node_modules/next/dist/bin/next",
+//         args: "start -p 5000",
+//         instances: 1,
+//         autorestart: true,
+//         watch: false,
+//         max_memory_restart: "1G",
+//         env: {
+//           NODE_ENV: "development",
+//         },
+//         env_production: {
+//           NODE_ENV: "production",
+//         },
+//       },
+//     ],
+//   };
+  
+
+
+  module.exports = {
     apps: [
       {
         name: "xchat-front-office",
-        script: "node_modules/next/dist/bin/next",
+        script: "node_modules/.bin/next",
         args: "start -p 5000",
+        cwd: "/root/chat-app/user",
+        env: {
+          NODE_ENV: "production",
+        },
         instances: 1,
         autorestart: true,
         watch: false,
         max_memory_restart: "1G",
-        env: {
-          NODE_ENV: "development",
-        },
-        env_production: {
-          NODE_ENV: "production",
-        },
       },
     ],
   };
+
+
   
